d44316081d23db7a0b85cd7b45dfd7d4df2b7658
Z Naddaf <znaddaf@protonmail.com>
2020-01-30 19:28:40 -0600

    Updated add functions to more intuitively present role titles and department names

--- index.js
+++ index.js
@@ -133,38 +133,54 @@ function addDep() {
 // -- Add Role Information
 // -------------------------------------------
 function addRole() {
-    inquirer.prompt(
-        [
-            {
-                type: "input",
-                name: "title",
-                message: "What is this role's title?"
-            },
-            {
-                type: "input",
-                name: "salary",
-                message: "What is this role's salary?"
-            },
-            {
-                type: "input",
-                name: "department_id",
-                message: "What is this role's department?"
-            },
-        ]
 
-    ).then(data => {
-        console.log("Creating a new role...\n");
-        var query = connection.query(
-            "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", [data.title, data.salary, data.department_id],
-            function (err, res) {
-                connection.query("SELECT * FROM roles", function (err, db_data) {
-                    console.table(db_data)
-                    addInfo();
-                })
-                if (err) throw err;
-                console.log("role created!\n");
+    // Here we prepare the questions by gathering the department names for future use
+    connection.query("SELECT * FROM departments", function (err, res) {
+        var department_names = []
+        for (let i = 0; i < res.length; i++) {
+            department_names.push(res[i].name)
+        }
+
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    name: "title",
+                    message: "What is this role's title?"
+                },
+                {
+                    type: "input",
+                    name: "salary",
+                    message: "What is this role's salary?"
+                },
+                {
+                    type: "list",
+                    name: "department_id",
+                    message: "What is this role's department?",
+                    choices: department_names
+                },
+            ]
+
+        ).then(data => {
+            console.log("Creating a new role...\n");
+            var queryId
+            for (let i = 0; i < res.length; i++) {
+                if (res[i].name === data.department_id) {
+                    queryId = res[i].id
+                }
             }
-        )
+            var query = connection.query(
+                "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", [data.title, data.salary, queryId],
+                function (err, res) {
+                    connection.query("SELECT * FROM roles", function (err, db_data) {
+                        console.table(db_data)
+                        addInfo();
+                    })
+                    if (err) throw err;
+                    console.log("role created!\n");
+                }
+            )
+        })
     })
 }
 
@@ -172,45 +188,61 @@ function addRole() {
 // -- Add Employee Information
 // -------------------------------------------
 function addEmp() {
-    inquirer.prompt(
-        [{
-            type: "input",
-            name: "first_name",
-            message: "What is this emplyee's first name?"
-        },
-        {
-            type: "input",
-            name: "last_name",
-            message: "What is this emplyee's last name?"
-        },
-        {
-            type: "input",
-            name: "role_id",
-            message: "What is this emplyee's role?"
-        },
-        {
-            type: "input",
-            name: "manager_id",
-            message: "What is the ID of this employee's Manager?"
-        }]
-
-    ).then(data => {
-        console.log("Creating a new Employee...\n");
-        var manager_id = null
-        if (data.manager_id) {
-            manager_id = data.manager_id
+    // Here we prepare the questions by gathering the role names for future use
+    connection.query("SELECT * FROM roles", function (err, res) {
+        var role_names = []
+        for (let i = 0; i < res.length; i++) {
+            role_names.push(res[i].title)
         }
-        connection.query(
-            "INSERT INTO employees (first_name, last_name, role_id, manager_id) Values (?, ?, ?, ?)", [data.first_name, data.last_name, data.role_id, manager_id],
-            function (err, res) {
-                connection.query("SELECT * FROM employees", function (err, db_data) {
-                    console.table(db_data)
-                    addInfo();
-                })
-                if (err) throw err;
-                console.log("employee created!\n");
+        console.log("Res check: ", res)
+
+        inquirer.prompt(
+            [{
+                type: "input",
+                name: "first_name",
+                message: "What is this emplyee's first name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is this emplyee's last name?"
+            },
+            {
+                type: "list",
+                name: "role_id",
+                message: "What is this emplyee's role?",
+                choices: role_names
+            },
+            {
+                type: "input",
+                name: "manager_id",
+                message: "What is the ID of this employee's Manager?"
+            }]
+
+        ).then(data => {
+            console.log("Creating a new Employee...\n");
+            var queryId
+            for (let i = 0; i < res.length; i++) {
+                if (res[i].title === data.role_id) {
+                    queryId = res[i].id
+                }
             }
-        )
+            var manager_id = null
+            if (data.manager_id) {
+                manager_id = data.manager_id
+            }
+            var query = connection.query(
+                "INSERT INTO employees (first_name, last_name, role_id, manager_id) Values (?, ?, ?, ?)", [data.first_name, data.last_name, queryId, manager_id],
+                function (err, res) {
+                    connection.query("SELECT * FROM employees", function (err, db_data) {
+                        console.table(db_data)
+                        addInfo();
+                    })
+                    if (err) throw err;
+                    console.log("employee created!\n");
+                }
+            )
+        })
     })
 }
 
